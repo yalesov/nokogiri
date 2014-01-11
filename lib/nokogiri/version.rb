20c0a911283f3d412245142c356625f2fa37f418
Cédric Boutillier <boutil@debian.org>
2014-01-11 06:25:20 +0100

    Imported Upstream version 1.6.1+ds

--- lib/nokogiri/version.rb
+++ lib/nokogiri/version.rb
@@ -1,6 +1,6 @@
 module Nokogiri
   # The version of Nokogiri you are using
-  VERSION = '1.6.0'
+  VERSION = '1.6.1'
 
   class VersionInfo # :nodoc:
     def jruby?
@@ -12,7 +12,7 @@ module Nokogiri
     end
 
     def loaded_parser_version
-      LIBXML_PARSER_VERSION.scan(/^(.*)(..)(..)$/).first.collect{ |j|
+      LIBXML_PARSER_VERSION.scan(/^(\d+)(\d\d)(\d\d)(?!\d)/).first.collect{ |j|
         j.to_i
       }.join(".")
     end
